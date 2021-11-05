f8439624c9291735cc8955b79838d3395c611605
Patryk Odziomek <patryk.odziomek@4soft.co>
2021-11-05 09:55:10 +0000

    added deployment scripts. Refactored tests. Added test helpers. Refactored directories.

--- test/colony_token.js
+++ test/colony_token.js
@@ -1,45 +1,45 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { setupGovernanceToken } = require("../scripts/setupContracts");
+const { toTokens } = require("./utils/testHelpers")
+const {exp} = require("mathjs");
+
+let colonyGovernanceToken;
+let owner, addr1, addr2;
 
 describe("Colony Token Base", function () {
+
+  before(async() => {
+    [owner, addr1, addr2] = await ethers.getSigners()
+    colonyGovernanceToken = await setupGovernanceToken()
+  })
+
   it("Token basic properties", async function () {
-    const [owner] = await ethers.getSigners()
-
-    const Colony = await ethers.getContractFactory("Colony")
-    const colony = await Colony.deploy()
-    await colony.deployed()
-
-    expect(await colony.name()).to.equal("Colony")
-    expect(await colony.symbol()).to.equal("CLY")
-
+    expect(await colonyGovernanceToken.name()).to.equal("Colony")
+    expect(await colonyGovernanceToken.symbol()).to.equal("CLY")
 
     const expectedSupply = 150 * 1000000
-    const decimals = await colony.decimals()
-    const totalSupply = await colony.totalSupply()
+    const decimals = await colonyGovernanceToken.decimals()
+    const totalSupply = await colonyGovernanceToken.totalSupply()
+    const expectedSupplyStr = toTokens(expectedSupply, decimals)
 
-    const expectedSupplyStr = expectedSupply.toString() + '0'.repeat(decimals)
 
     expect(decimals).to.equal(18)
-    expect(totalSupply).to.equal(expectedSupplyStr)
-    expect(totalSupply).to.equal(await colony.balanceOf(owner.address))
-  });
+    expect(totalSupply.toString()).to.equal(expectedSupplyStr)
+    expect(totalSupply).to.equal(await colonyGovernanceToken.balanceOf(owner.address))
+  })
 
   it("Token simple transfer", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners()
-
-    const Colony = await ethers.getContractFactory("Colony")
-    const colony = await Colony.deploy()
-    await colony.deployed()
-
     const amount = "12345"
-    const tx = await colony.connect(owner).transfer(addr1.address, amount)
+    const tx = await colonyGovernanceToken.connect(owner).transfer(addr1.address, amount)
     await tx.wait()
-    expect(await colony.balanceOf(addr1.address)).to.equal(amount)
+    expect(await colonyGovernanceToken.balanceOf(addr1.address)).to.equal(amount)
 
-    await colony.connect(addr1).transfer(addr2.address, amount)
-    expect(await colony.balanceOf(addr2.address)).to.equal(amount)
+    await colonyGovernanceToken.connect(addr1).transfer(addr2.address, amount)
+    expect(await colonyGovernanceToken.balanceOf(addr2.address)).to.equal(amount)
 
-    await expect(colony.connect(addr1).transfer(addr2.address, amount))
+    await expect(colonyGovernanceToken.connect(addr1).transfer(addr2.address, amount))
       .to.be.revertedWith("ERC20: transfer amount exceeds balance'")
-  });
-});
+  })
+
+})
